7b543cc649086553d52b45de4d8e594bbe065eb2
Khomych1004 <khomych100488@gmail.com>
2024-03-28 16:01:48 +0000

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,14 @@ import "./footer.css";
 
 const Footer = () => {
     return (
-        <div class="container-fluid" style={{ textAlign: 'center', backgroundColor:'gray' }}>
-            <p>Oleksandr Khomych &copy; {new Date().getFullYear()}</p>
-        </div>
+
+        <footer className="footer container-fluid" style={{ backgroundColor: '#f8f9fa' }}>
+            <div className="row justify-content-center">
+                <div className="col-md-6 text-center">
+                    <p>Oleksandr Khomych &copy; {new Date().getFullYear()}</p>
+                </div>
+            </div>
+        </footer>
     )
 }
 
