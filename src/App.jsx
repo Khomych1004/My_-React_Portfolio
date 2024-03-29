5f2d50c61bef0e7d0fdc5f06021f8ce2d685272d
Khomych1004 <khomych100488@gmail.com>
2024-03-29 02:04:07 +0000

    test link foto

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
 import Projects from './components/projects/Projects.jsx';
-import Project from './components/project/Project.jsx';
+import ViewProject from './components/viewProject/ViewProject.jsx';
 import Contact from './components/contact/Contact.jsx';
 import Footer from './components/footer/Footer.jsx';
 //import Resume from './components/resume/Resume';
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/*" element={<Home />} />
         <Route path="/Projects" element={<Projects />} />
-        <Route path="/Projects/:id" element={<Project />} />
+        <Route path="/Projects/:id" element={<ViewProject />} />
         <Route path="/Contact" element={<Contact />} />
         {/* <Route path="/Resume" element={<Resume />} /> */}
       </Routes>
